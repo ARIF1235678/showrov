c780999fe9af5eb50893288f5aca1d1fbf6d56a8
Arif YT <160728429+ARIF1235678@users.noreply.github.com>
2024-05-02 11:32:18 +0600

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+        strings: [
+            "<span>Hi, I am Arif</span> As a freelancer skilled in web development, web design, and graphic design, you possess a diverse set of talents crucial in today's digital landscape. Your ability to seamlessly blend aesthetics with functionality enables you to craft visually stunning and user-friendly websites. <br><br> With your expertise, clients can expect top-notch, bespoke solutions tailored to their specific needs, ensuring their online presence stands out in a crowded market. Your dedication to mastering these disciplines positions you as a valuable asset in the ever-evolving realm of digital creativity."
+        ],
+        typeSpeed: 7,
+        contentType: 'html'
+    });
+}, 100);
